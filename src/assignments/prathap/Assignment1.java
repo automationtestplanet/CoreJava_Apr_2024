ca85c7d5c9668e7d9b38089042a1a68d2dbe34f0
RAJU CHELLE <rajuchelle@gmail.com>
2024-04-30 06:54:42 +0300

    assignment1

---
+++ src/assignments/prathap/Assignment1.java
@@ -0,0 +1,12 @@
+package assignments.prathap;
+
+public class Assignment1 {
+
+	// Write program to count the number of repeating words and repeating characters
+	// form the each work of a given sting (Sentence)
+
+	public static void main(String[] args) {
+
+	}
+
+}
