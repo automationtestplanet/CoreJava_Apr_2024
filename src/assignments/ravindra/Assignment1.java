7ca0734366f4e6b2bdd516a067ed789ef7310385
Your Name <you@example.com>
2024-05-01 19:11:10 +0530

    [200~

--- src/assignments/ravindra/Assignment1.java
+++ src/assignments/ravindra/Assignment1.java
@@ -3,7 +3,25 @@ package assignments.ravindra;
 public class Assignment1 {
 
 //	Write a reusable method print Even and Odd numbers from the given range
-	public static void main(String[] args) {
-		
+	
+	
+    public static void reusableMethodPrintEvenAndOddNumbersFromTheGivenRange(int start, int end) {
+		System.out.println("Even numbers:");
+		for (int i = start; i <= end; i++) {
+		    if (i % 2 == 0) {
+		        System.out.print(i + " ");
+		    }
+		}
+		System.out.println("\nOdd numbers:");
+		for (int i = start; i <= end; i++) {
+		    if (i % 2 != 0) {
+		        System.out.print(i + " ");
+		    }
+		}
+	}
+    public static void main(String[] args) {
+    	reusableMethodPrintEvenAndOddNumbersFromTheGivenRange(1,50); 
 	}
 }
+
+
