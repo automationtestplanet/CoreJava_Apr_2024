7e414eed720fb475b65118440c512974834acb1a
Srikanth <kukutlapallysrikanth@gmail.com>
2024-05-01 19:16:06 +0530

    Assignment1

--- src/assignments/ksrikanth/Assignment1.java
+++ src/assignments/ksrikanth/Assignment1.java
@@ -3,7 +3,24 @@ package assignments.ksrikanth;
 public class Assignment1 {
 
 //	Write a reusable method print Even and Odd numbers from the given range 
+
+	public static void printEvenAndOddNum(int a, int b) {
+		System.out.println("Odd Numbers:");
+		for (int i = a; i <= b; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+		System.out.println("Even Numbers");
+		for (int i = a; i <= b; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		
+		printEvenAndOddNum(1, 10);
 	}
 }
