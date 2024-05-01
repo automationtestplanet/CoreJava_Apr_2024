f3a24ef0f0ec837a046467b7baaf7a81e00af260
Eswar Chalapati <pitch@Eswar.com>
2024-05-01 17:07:10 +0530

    assignment1

--- src/assignments/prabandha/Assignment1.java
+++ src/assignments/prabandha/Assignment1.java
@@ -2,8 +2,22 @@ package assignments.prabandha;
 
 public class Assignment1 {
 
+	private static final int i = 0;
 	//Write a reusable method check whether the string is Pallendrome or not
-	public static void main(String[] args) {
+	public static  boolean Pallendrome(String str ){
+		
+		int n= str.length();
+		
+		//Compare characters from beginning and end 
+		
+		
+		for( int i= 0;i<n/2;i++)
+			
+			if (str.charAt(i)!= str.charAt(n-i-1))
+				
+				return false;
+		return true;
 		
 	}
-}
+	}
+
