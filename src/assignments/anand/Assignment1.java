c6870ae67d8cab68baef2c43330fdb6c3d89e506
Anandvaddadi <anandvaddadi3234@gmail.com>
2024-05-02 00:19:00 -0400

    Assignment1

--- src/assignments/anand/Assignment1.java
+++ src/assignments/anand/Assignment1.java
@@ -5,7 +5,23 @@ public class Assignment1 {
 	
 	//Write a reusable method check whether the string is Pallendrome or not
 
-	public static void main(String[] args) {
-		
-	}
+
+    public static void main(String[] args) {
+        String str = "madam";
+        if(isPalindrome(str))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
