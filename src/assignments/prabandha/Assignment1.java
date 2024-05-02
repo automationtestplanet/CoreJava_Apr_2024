b1bdd854ffc312be9366a789a90176ada7c5e94a
RAJU CHELLE <rajuchelle@gmail.com>
2024-05-02 07:56:22 +0530

    new changes

--- src/assignments/prabandha/Assignment1.java
+++ src/assignments/prabandha/Assignment1.java
@@ -2,22 +2,20 @@ package assignments.prabandha;
 
 public class Assignment1 {
 
+	// Write a reusable method check whether the string is Pallendrome or not
+	
 	private static final int i = 0;
-	//Write a reusable method check whether the string is Pallendrome or not
-	public static  boolean Pallendrome(String str ){
-		
-		int n= str.length();
-		
-		//Compare characters from beginning and end 
-		
-		
-		for( int i= 0;i<n/2;i++)
-			
-			if (str.charAt(i)!= str.charAt(n-i-1))
-				
+	
+	public static boolean Pallendrome(String str) {
+		int n = str.length();
+		for (int i = 0; i < n / 2; i++)
+			if (str.charAt(i) != str.charAt(n - i - 1))
 				return false;
 		return true;
-		
 	}
+	
+	public static void main(String[] args) {
+		System.out.println(Pallendrome("AbcbA"));
+		System.out.println(Pallendrome("AbcaB"));
 	}
-
+}
