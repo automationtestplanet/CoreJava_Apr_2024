378daa8f5670df76fabba768eeecc7984e76757a
RAJU CHELLE <rajuchelle@gmail.com>
2024-05-02 07:44:24 +0530

    new changes

--- src/assignments/anand/Assignment1.java
+++ src/assignments/anand/Assignment1.java
@@ -4,6 +4,8 @@ package assignments.anand;
 public class Assignment1 {
 	
 	//Write a reusable method check whether the string is Pallendrome or not
+	// i/p: AbcbA  -> AbcbA
+	// AbcaB  -> BacbA
 
 	public static void main(String[] args) {
 		
