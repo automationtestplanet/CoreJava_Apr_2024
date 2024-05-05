bcbcdb67adb2f1a432c44bcca3559a506a1220f0
Shiva <sivasv89@gmail.com>
2024-05-05 16:17:52 +0530

    assingment1siva

--- src/assignments/sivasurya/Assignment1.java
+++ src/assignments/sivasurya/Assignment1.java
@@ -2,8 +2,34 @@ package assignments.sivasurya;
 
 public class Assignment1 {
 
-	//Write a reusable method check whether the string is Pallendrome or not
+
+	public static void testPalindrome(String s)
+	{
+		int strLength = s.length();
+		String revStr = "";
+	    for (int i = (strLength - 1); i >=0; --i) {
+	      revStr = revStr + s.charAt(i);
+	    }
+
+	    if (s.toLowerCase().equals(revStr.toLowerCase())) {
+	      System.out.println("The given String :" + s + " is palindrome");
+	    }
+	    else {
+	      System.out.println("The given String :" + s + " is not palindrome");
+	    }
+	}
+	
 	public static void main(String[] args) {
 		
-	}
+		
+		String str = "level";
+	    
+		Assignment1 tp =new Assignment1();
+		
+		tp.testPalindrome(str);
+		
+	
 }
+}
+
+
