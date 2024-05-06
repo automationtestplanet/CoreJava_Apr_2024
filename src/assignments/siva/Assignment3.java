8436199eb6893ff59610019d4c044475af7a726c
sivav51 <sivavallabhasetti51@gmail.com>
2024-05-06 13:51:34 +0530

    siva_vallabahsetti_assignment3 completed

--- src/assignments/siva/Assignment3.java
+++ src/assignments/siva/Assignment3.java
@@ -1,7 +1,34 @@
 package assignments.siva;
 
+import java.util.Arrays;
+
 public class Assignment3 {
 
-//	Write a program to print No of owels availble in each word in given string
-//	Ex: "Hello this is Java" -> Hello -> 2, this-1, is-1, Java-2 
+//		Write a program to print No of vowels available in each word in given string
+//		Ex: "Hello this is Java" -> Hello -> 2, this-1, is-1, Java-2 
+	public static void main(String[] args) {
+
+		String s = "Hello this is Java";
+		int count = 0;
+		System.out.println(s);
+		String a[] = s.split(" ");
+
+		for (int i = 0; i <= a.length - 1; i++) {
+			for (int j = 0; j < a[i].length(); j++) {
+
+				if (a[i].charAt(j) == 'a' || a[i].charAt(j) == 'e' || a[i].charAt(j) == 'i' || a[i].charAt(j) == 'o'
+						|| a[i].charAt(j) == 'u') {
+
+					count++;
+				}
+
+			}
+			System.out.println(a[i] + "  no of vowels available: " + count);
+
+			count = 0;
+
+		}
+		
+
+	}
 }
