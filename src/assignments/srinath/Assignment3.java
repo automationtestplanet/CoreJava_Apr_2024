4ffffa1f3d44f9ae427ed524cbefe893ce10627d
srinath <jonnada.srinathmudiraj@gmail.com>
2024-05-15 17:36:43 +0530

    assigment

--- src/assignments/srinath/Assignment3.java
+++ src/assignments/srinath/Assignment3.java
@@ -7,4 +7,39 @@ public class Assignment3 {
 //	Ex: 1 to 10 -> 0,1,1,2,3,5,8
 //		1 to 20 -> 0,1,1,2,3,5,8,13
 
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 1;
+		int c;
+		int i;
+		int count = 7;
+		System.out.print("1 to 10 ->");
+		System.out.print(a + " , " + b);
+
+		for (i = 2; i < count; ++i) {
+			c = a + b;
+			System.out.print(" , " + c);
+			a = b;
+			b = c;
+		}
+
+		int d = 0;
+		int e = 1;
+		int f;
+		int j;
+		int count1 = 8;
+		System.out.println(" ");
+		System.out.print("1 to 20 ->");
+		System.out.print(d + " , " + e);
+
+		for (j = 2; j < count1; ++j) {
+			f = d + e;
+			System.out.print(" , " + f);
+			d = e;
+			e = f;
+		}
+
+	}
+
 }
